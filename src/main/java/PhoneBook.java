8765f01c27ddc603ecab63247ee38e542e21b9bd
AZharick <bopohlabs@mail.ru>
2023-09-07 22:48:56 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,2 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+   private Map<String, String> map = new HashMap<>();
+
+   public int add(String name, String phoneNumber) {
+      return 0;
+   }
+
+   public int getListSize() {
+      return map.size();
+   }
+
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PhoneBookTest {
+
+   @Test
+   public void testAdd_contactsCounter() {
+      PhoneBook book = new PhoneBook();
+      final String name = "Alex";
+      final String number = "8-999-111-22-33";
+      book.add(name, number);
+
+      final int expected = 1;
+      final int factResult = book.getListSize();
+      Assertions.assertEquals(expected, factResult);
+   }
 }
